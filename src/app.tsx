34e37ce6eeccf03885eb82a777874c6a5c05e06c
Cameron Clough <cameronjclough@gmail.com>
2024-06-09 23:13:57 +0100

    theme provider

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,6 @@
 import { Component, lazy, Suspense } from 'solid-js'
 import { Router, Route } from '@solidjs/router'
+import { ColorModeProvider, ColorModeScript, createLocalStorageManager } from '@kobalte/core'
 
 const Login = lazy(() => import('./pages/auth/login'))
 const Logout = lazy(() => import('./pages/auth/logout'))
@@ -7,10 +8,16 @@ const Auth = lazy(() => import('./pages/auth/auth'))
 const Dashboard = lazy(() => import('./pages/dashboard'))
 
 const App: Component = () => {
+  const storageManager = createLocalStorageManager('vite-ui-theme')
   return (
     <Router
       root={(props) => (
-        <Suspense>{props.children}</Suspense>
+        <>
+          <ColorModeScript storageType={storageManager.type} />
+          <ColorModeProvider storageManager={storageManager}>
+            <Suspense>{props.children}</Suspense>
+          </ColorModeProvider>
+        </>
       )}
     >
       <Route path="/login" component={Login} />
